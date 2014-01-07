daa08c81a6bcda98fc0741f3521b944eeb8d4457
WilliamHua <william.hua@utexas.edu>
2014-01-07 00:29:23 -0600

    Finished calculating prior corr and stddev matrix

--- black_litterman/blacklitterman.py
+++ black_litterman/blacklitterman.py
@@ -1,3 +1,9 @@
+import yfinance
+import numpy as np
+import pandas as pd
+import math
+from scipy.stats.stats import pearsonr
+
 def calc_mean(data):
     return sum(data)/len(data)
 
@@ -8,22 +14,44 @@ def stddev(data):
         summation += (x - mean) ** 2
     return math.sqrt(summation/len(data))
 
-def correl_matrix(assets):
-    corr_matrix = np.zeroes(len(assets), len(assets))
-    for i in range(len(assets)):
-        for j in range(len(assets)):
-            corr_matrix[i][j] = numpy.correlate(assets[i], assets[j])
-            corr_matrix[j][i] = numpy.correlate(assets[i], assets[j])
+def correl_matrix(assets, tickers):
+    corr_matrix = np.zeros((len(assets.columns), len(assets.columns)))
+    for i in range(len(assets.columns)):
+        for j in range(len(assets.columns)):
+            corr_matrix[i][j] = pearsonr(assets[tickers[i]], 
+                    assets[tickers[j]])[0]
+            corr_matrix[j][i] = pearsonr(assets[tickers[i]], 
+                    assets[tickers[j]])[0]
 
     return corr_matrix 
 
-def stddev_matrix(assets):
+def stddev_matrix(assets, tickers):
     #assumes len(assets) = num of assets
     #len(assets[0]) = length of time series
-    dev_matrx = np.zeros(len(assets), len(assets))
-    for x in range(len(assets)):
-        dev_matrix[x][x] = stddev(assets[x] * 16) #annualized, 256 days in a year
+    dev_matrix = np.zeros((len(assets.columns), len(assets.columns)))
+    for x in range(len(assets.columns)):
+        dev_matrix[x][x] = stddev(assets[tickers[x]])*16 #annualized, 256 days in a year
 
     return dev_matrix
 
+tickers = ["aapl", "cly", "dbb", "eem"]#, "emb", "fxe", "gld", "goog", "gs", 
+#    "hyg", "ibm", "iyr", "tlt", "ung", "uso", "vxx", "wmt", "x", "xiv", "xom"]
+prefix = "data/"
+postfix = ".csv"
+weights = np.array([.25147, .00014, .00013, .02181, .00186, .00011, .01745,
+        .19181, .04226, .00826, .10378, .00206, .00123, .00047, .00048, .00054, 
+        .13004, .00220, .00018, .22372])
+
+returns = {}
+for x in tickers:
+    stock = yfinance.parse_yfin(prefix + x + postfix)
+    returns.update({x:stock})
+
+returns = pd.DataFrame(returns, columns=tickers)
+dev_matrix = stddev_matrix(returns, tickers)
+corr_matrix = correl_matrix(returns, tickers)
+
+#print(returns["xiv"][0])
+print(calc_mean(returns["aapl"])*250)
+print(calc_mean(returns["cly"])*250)
 
--- black_litterman/yfinance.py
+++ black_litterman/yfinance.py
@@ -2,7 +2,6 @@
 
 import pandas as pd 
 import numpy as np
-import math
 
 
 def parse_yfin(file_name):
@@ -28,20 +27,10 @@ def parse_yfin(file_name):
 
     return returns#, date[1:]
 
-def add_returns(data, returns, asset_name):
-    """
-    Takes a DataFrame and adds another column of returns to it
-
-    data:       the DataFrame
-    returns:    list of returns 
-    asset_name: index of returns
-    """
-    data[asset_name] = pd.Series(returns, index=data.index)
-
 #FIXME:CALCULATE RISK FREE RETURNS
 def calc_returns(price_series):
     """
-    Calculates a return given a list of prices
+    Helper method to calculate a return given a list of prices
 
     price_series:       list of prices
     output:             list of returns
@@ -51,34 +40,6 @@ def calc_returns(price_series):
         returns.append((price_series[x + 1] - price_series[x]) /\
                 price_series[x]) 
     return returns
-
-def calc_mean(data):
-    return sum(data)/len(data)
-
-def stddev(data):
-    mean = calc_mean(data)
-    summation = 0
-    for x in data:
-        summation += (x - mean) ** 2
-    return math.sqrt(summation/len(data))
-
-def correl_matrix(assets):
-    corr_matrix = np.zeroes(len(assets), len(assets))
-    for i in range(len(assets)):
-        for j in range(len(assets)):
-            corr_matrix[i][j] = numpy.correlate(assets[i], assets[j])
-            corr_matrix[j][i] = numpy.correlate(assets[i], assets[j])
-
-    return corr_matrix 
-
-def stddev_matrix(assets):
-    #assumes len(assets) = num of assets
-    #len(assets[0]) = length of time series
-    dev_matrx = np.zeros(len(assets), len(assets))
-    for x in range(len(assets)):
-        dev_matrix[x][x] = stddev(assets[x] * 16) #annualized, 256 days in a year
-
-    return dev_matrix
 
 #-------- possible will remove
 
@@ -103,23 +64,4 @@ def format_returns(date, returns, asset_name):
 
 
         
-tickers = ["aapl", "cly", "dbb", "eem", "emb", "fxe", "gld", "goog", "gs", 
-    "hyg", "ibm", "iyr", "tlt", "ung", "uso", "vxx", "wmt", "x", "xiv", "xom"]
-prefix = "data/"
-postfix = ".csv"
-weights = np.array([.25147, .00014, .00013, .02181, .00186, .00011, .01745,
-        .19181, .04226, .00826, .10378, .00206, .00123, .00047, .00048, .00054, 
-        .13004, .00220, .00018, .22372])
-
-returns = {}
-for x in tickers:
-    stock = parse_yfin(prefix + x + postfix)
-    print(x + " " + str(len(stock)))
-    returns.update({x:stock})
-
-returns = pd.DataFrame(returns, columns=tickers)
-
-print(returns["xiv"][0])
-print(calc_mean(returns["aapl"])*250)
-print(calc_mean(returns["cly"])*250)
 
